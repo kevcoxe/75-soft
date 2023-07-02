29ff4925f610e9ae9ba610dd8aa79a6d0302d75d
Kevin Coxe <kevcoxe@gmail.com>
2023-07-02 01:59:53 -0400

    add todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,10 +50,68 @@ export default async function Home() {
 }
 */
 
-export default async function Home () {
+import Login from '@/app/login'
+import NewTodo from '@/app/new-post'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { revalidatePath } from 'next/cache'
+import { cookies } from 'next/headers'
+
+export default async function ServerComponent() {
+
+  const toggleTaskComplete = async (formData: FormData) => {
+    "use server"
+
+    const taskId = formData.get('taskId')
+    const isComplete = String(formData.get('isComplete'))
+
+    const supabase = createServerComponentClient<Database>({ cookies })
+    const {
+      data: { session }
+    } = await supabase.auth.getSession()
+
+    if (!session) return
+    const { data, error } = await supabase
+      .from('todos')
+      .update({ 
+        is_complete: !JSON.parse(isComplete),
+        user_id: session?.user.id
+      })
+      .eq('id', taskId)
+  
+    if (error) {
+      console.error(error)
+    }
+
+    revalidatePath('/')
+  }
+
+  const supabase = createServerComponentClient<Database>({ cookies })
+  const { data: todos } = await supabase.from('todos').select()
+
+  const todoList = todos?.map(({ id, task, is_complete }, i) => {
+    return (
+      <div key={i}>
+        <form action={toggleTaskComplete}>
+          <button type='submit'>
+            <input type="hidden" defaultValue={ id } name="taskId" />
+            <input type="hidden" defaultValue={ JSON.stringify(is_complete) } name="isComplete" />
+            <span>{ is_complete ? '✅' : '' } { task }</span>
+          </button>
+        </form>
+      </div>
+      )
+    })
+
   return (
-    <div>
-      hello
-    </div>
+    <>
+      <Login />
+      <NewTodo />
+      { todos && (
+        <div className='flex flex-col'>
+          <h1>Todos: </h1>
+          { todoList }
+        </div>
+      )}
+    </>
   )
 }
