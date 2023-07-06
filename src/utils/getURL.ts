ef87ea30f9a89f52cdde308b6a4ce1cee9ced322
Kevin Coxe <kevcoxe@gmail.com>
2023-07-05 21:16:20 -0400

    Update getURL.ts

--- src/utils/getURL.ts
+++ src/utils/getURL.ts
@@ -4,9 +4,5 @@ export default function getURL(path: string) {
   const baseURL = IS_SERVER
     ? process.env.NEXT_PUBLIC_SITE_URL!
     : window.location.origin;
-
-  console.log(`path: ${path}`)
-  console.log(`baseURL: ${baseURL}`)
-
   return new URL(path, baseURL).toString();
 }
