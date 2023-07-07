fbfe2eb3bcb554e7791013cd732a73c86b093b69
Kevin Coxe <kevcoxe@gmail.com>
2023-07-07 01:13:24 -0400

    fix mile tracker

--- src/app/components/client/Miles.tsx
+++ src/app/components/client/Miles.tsx
@@ -13,27 +13,32 @@ export default function Miles({
 
   const supabaseContext = UseSupabaseContext()
 
-  const [ milesTracked, setMiles ] = useState<number>(profile.miles_walked)
+  const [ milesTracked, setMiles ] = useState<number>()
   const debouncedMiles = useDebounce(milesTracked, 500)
+
+  useEffect(() => {
+    setMiles(profile.miles_walked)
+  }, [profile.miles_walked])
 
   // wait for user to stop clicking before submitting
   useEffect(() => {
     if (!supabaseContext) return
     if (debouncedMiles !== undefined) {
-      const updateMiles = async (mileCount: number) => {
-        const scoreChange = (mileCount - profile.miles_walked) * POINT_PER_MILE
+      const updateMiles = async () => {
+        const score_increment = (debouncedMiles - profile.miles_walked) * POINT_PER_MILE
+        const mile_increment = debouncedMiles - profile.miles_walked
         const { error } = await supabaseContext
           .rpc('incrementMiles', {
             userid: profile.user_id,
-            score_increment: scoreChange,
-            mile_increment: mileCount
+            score_increment,
+            mile_increment
           })
 
         if (error) {
           console.log(error)
         }
       }
-      updateMiles(debouncedMiles)
+      updateMiles()
     }
   }, [debouncedMiles])
 
@@ -48,11 +53,11 @@ export default function Miles({
   return (
     <>
       <div className="flex flex-col p-4 m-2 border rounded-lg border-slate-800">
-        <h1 className="text-xl">Mile tracker: { milesTracked !== undefined ? milesTracked : "loading..." }</h1>
+        <h1 className="mb-2 text-xl">Miles Walked: { milesTracked !== undefined ? milesTracked : "loading..." }</h1>
 
         <div className="grid grid-cols-2">
-          <button onClick={decrementMiles} className="col-span-1 text-center bg-red-600 rounded-l-lg">-</button>
-          <button onClick={incrementMiles} className="col-span-1 text-center bg-green-600 rounded-r-lg ">+</button>
+          <button onClick={decrementMiles} className="col-span-1 py-1 text-center bg-red-600 rounded-l-lg">-</button>
+          <button onClick={incrementMiles} className="col-span-1 py-1 text-center bg-green-600 rounded-r-lg ">+</button>
         </div>
       </div>
     </>
