8da16ffc452104462b8054b9e4d0dadf0f408ab6
Kevin Coxe <kevcoxe@gmail.com>
2023-07-21 02:11:47 -0400

    Update Stats.tsx

--- src/app/components/client/Stats.tsx
+++ src/app/components/client/Stats.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { BiShow, BiHide } from "react-icons/bi"
 import ProgressTracker from "@/app/components/client/ProgressTracker"
 import { motion } from "framer-motion"
 
@@ -32,24 +31,21 @@ export default function Stats({
       animate={{ opacity: 1 }}
       transition={{ delay: .3 }}
     >
-      <div className="flex flex-col p-4 m-2 border rounded-lg border-slate-800">
-        <div className="grid items-center grid-cols-6">
-          {/* <div className="flex col-span-6">
-            <div className="w-24 h-24 rounded-full bg-slate-800"></div>
-          </div> */}
-          <div className="flex flex-col col-span-6">
-            <div className="grid grid-cols-6">
-              <div className="col-span-5 px-2 py-4 my-2 text-2xl font-bold rounded-xl">Username: { userProfile.username }</div>
-              <button className="col-span-1 mx-auto text-4xl" onClick={handleCollapse}>{ collapse ? <BiShow /> : <BiHide /> }</button>
-            </div>
-            <div className={`${collapse ? "hidden" : ""}`}>
-              <ProgressTracker profile={userProfile} />
-              <div className="px-2 py-4 my-2 text-2xl font-bold rounded-xl">Score: { userProfile.score }</div>
-              <div className="px-2 py-4 my-2 text-2xl font-bold rounded-xl">Days Completed: { userProfile.days_sucessful }</div>
-              <div className="px-2 py-4 my-2 text-2xl font-bold rounded-xl">Total Miles walked: { userProfile.miles_walked }</div>
-              { logoutChild }
-            </div>
-          </div>
+      <ProgressTracker profile={userProfile} />
+      <div className="w-full shadow stats stats-vertical">
+        <div className="stat place-items-center">
+          <div className="stat-title">Points</div>
+          <div className="stat-value">{ profile.score }</div>
+        </div>
+
+        <div className="stat place-items-center">
+          <div className="stat-title">Days Completed</div>
+          <div className="stat-value">{ profile.days_sucessful }</div>
+        </div>
+
+        <div className="stat place-items-center">
+          <div className="stat-title">Total Miles Walked</div>
+          <div className="stat-value">{ profile.miles_walked }</div>
         </div>
       </div>
     </motion.div>
